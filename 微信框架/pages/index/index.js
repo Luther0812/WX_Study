688a39b70d1d082fe900ccadd2951dc34590f085
Luther0812 <luther0812@163.com>
2017-02-08 11:57:20 +0800

    Foundation Study

--- 微信框架/pages/index/index.js
+++ 微信框架/pages/index/index.js
@@ -13,7 +13,16 @@ Page({
     staffA: {firstName: 'Hulk', lastName: 'Hu'},
     staffB: {firstName: 'Shang', lastName: 'You'},
     staffC: {firstName: 'Gideon', lastName: 'Lin'},
-    count: 1
+    count: 1,
+    objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ],
+    numberArray: [1, 2, 3, 4]
   },
   changeName : function(e){
     this.setData({
@@ -89,5 +98,35 @@ Page({
     this.setData({
       count: this.data.count + 1
     })
+  },
+  switch: function(e) {
+    const length = this.data.objectArray.length
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addToFront: function(e) {
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{id: length, unique: 'unique_' + length}].concat(this.data.objectArray)
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addNumberToFront: function(e){
+    this.data.numberArray = [ this.data.numberArray.length + 1 ].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
+  },
+  bindViewTap:function(event){
+    event.target.dataset.alphaBeta === 1 // - 会转为驼峰写法
+    event.target.dataset.alphabeta === 2 // 大写会转为小写
   }
 })
